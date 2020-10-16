edf6ddab7edd120c7b05482fd02cab58a43f106b
jiajie <jiajie.ma@thoughtworks.com>
2020-10-16 10:36:05 +0800

    finish practice1.js and practice2.js.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData = ( {data, column} ) => {
+    const output = [];
+
+    data.map( item => {
+            let member = {};
+            column.forEach(it => {
+                member[it.name] = item.shift();
+            })
+
+            output.push(member);
+        }
+    )
+
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
+const inject = (items, sections) => {
+    let items_copy = [...items];
+    sections.sort( (it1, it2) => it2.index - it1.index ).map(
+        it => {
+            items_copy.splice(it.index, 0, it.content);
+        }
+    );
+
+    return items_copy;
+}
 export { inject };
